8fcc79246da03ffb23754726e764532ad34da262
Abhishek Dhanke <abhishekrdhanke636@gmail.com>
2022-10-01 16:23:26 +0530

    feat : add footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,12 @@ const Home = () => {
         <p className='mainHead'>
             &nbsp;&nbsp;Trace🚀
         </p>
-        <p className='subhead'>Seamlessly Track your APIs</p>
+        <p className='subhead'>Seamlessly Track && Monitor your APIs</p>
+       <a href='https://github.com/abhishekrd/Trace' className='minihead'><p>Built with ❤️ by Abhishek</p></a>
+       <div class="card-social">
+               <a href="https://www.linkedin.com/in/abhishek-dhanke-226354216/"> <i class='bx bxl-linkedin-square'></i> </a>
+               <a href="https://github.com/abhishekrd"><i class='bx bxl-github' ></i></a> 
+            </div>
     </div>
   )
 }
